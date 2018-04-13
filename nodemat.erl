ef71e9242d6a1438270bd239630c10f1e1f63302
Saiteja-Reddy <saiteja0317@gmail.com>
2018-04-13 17:03:11 +0530

    get Cij Done - with function - working

--- nodemat.erl
+++ nodemat.erl
@@ -10,7 +10,7 @@
 %%     erl -noinput -s mat start
 
 -module(nodemat). 
--export([reverse_create/1, start/0, printMat/4, multiply/9, multiplyRC/10, multserver/0, domultiply/7, startmultserver/0, getSubMatrix/9, addMatrix/6, getCElement/11]). 
+-export([reverse_create/1, start/0, printMat/4, multiply/9, multiplyRC/10, multserver/0, domultiply/7, startmultserver/0, getSubMatrix/9, addMatrix/6, getCElement/13]). 
 -import(lists, [reverse/1, nth/2]).
 -import(math, [sqrt/1]).
 
@@ -97,22 +97,21 @@ addMatrix(M1,M2,C,R,X,SM) ->
    end.
 
 % do  contention-free formula here
-getCElement(M1,M2, R1, C1, R2, C2, P, I, J, K, Sum) -> % assume I = J, P = no. of Processors
-     KL = round(sqrt(P)),
+getCElement(M1,M2, R1, C1, R2, C2, KL, N, P, I1, J1, K, Sum) -> % assume I = J, P = no. of Processors
+     I = I1 - 1,
+     J = J1 - 1,
      if 
         (K == KL) ->
           Sum;       
         true -> 
-           R = I,
+           R = I + 1,
            S = ((I+J+K) rem KL) + 1,
-           N = R1/sqrt(P),
            A = getSubMatrix(M1, R1, C1, R, S, P, (R-1)*N, (S-1)*N , []),
-           R1 = ((I+J+K) rem KL) + 1,
-           S1 = J,
-           B = getSubMatrix(M2, R2, C2, R1, S1, P, (R1-1)*N, (S1-1)*N , []),
+           S1 = J + 1,
+           B = getSubMatrix(M2, R2, C2, S, S1, P, (S-1)*N, (S1-1)*N , []),
            MultAB = multiply(A,B,N,N,N,N,[], 1,1),
            Net = addMatrix(Sum,MultAB, N,N,1, []),
-           getCElement(M1,M2,R1,C1,R2,C2, P, I, J, K+1, Net)           
+           getCElement(M1,M2,R1,C1,R2,C2, KL, N, P, I1, J1, K+1, Net)           
      end.
 
 reverse_create( 0 ) -> [];
@@ -128,61 +127,25 @@ start() ->
     M2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64],
     printMat(M1,R1,C1,1),
 
-
-    % Aut = getCElement(M1,M2, R1, C1, R2, C2, P, 1, 1, 0, []),
-    % io:fwrite("~w~n",[Aut]).
-
     KL = round(sqrt(P)),
     N = round(R1/sqrt(P)),   
-
     Sum = reverse_create(N*N),
-    % io:fwrite("~w Here~n",[Sum]),
 
-    I = 1,
-    J = 2,
-    Aut = getSubMatrix(M1,R1,C1,I,J,P, (I-1)*N, (J-1)*N, []),
+    Aut = getCElement(M1,M2, R1, C1, R2, C2, KL, N, P, 1, 1, 0, Sum),
     printMat(Aut,N,N,1),
 
-    I1 = 2,
-    J1 = 1,
-    Aut1 = getSubMatrix(M2,R1,C1,I1,J1,P, (I1-1)*N, (J1-1)*N, []),
+    Aut1 = getCElement(M1,M2, R1, C1, R2, C2, KL, N, P, 1, 2, 0, Sum),
     printMat(Aut1,N,N,1),
 
-
-    MultAB = multiply(Aut,Aut1,N,N,N,N,[], 1,1),
-    printMat(MultAB,N,N,1),
-
-    Sum1 = addMatrix(Sum,MultAB, N,N,1, []),
-
-    I2 = 1,
-    J2 = (2 + 1) rem KL,
-    Aut2 = getSubMatrix(M1,R1,C1,I2,J2,P, (I2-1)*N, (J2-1)*N, []),
+    Aut2 = getCElement(M1,M2, R1, C1, R2, C2, KL, N, P, 2, 1, 0, Sum),
     printMat(Aut2,N,N,1),
 
-    I3 = (2 + 1) rem KL,
-    J3 = 1,
-    Aut3 = getSubMatrix(M2,R1,C1,I3,J3,P, (I3-1)*N, (J3-1)*N, []),
+    Aut3 = getCElement(M1,M2, R1, C1, R2, C2, KL, N, P, 2, 2, 0, Sum),
     printMat(Aut3,N,N,1),
 
-    MultAB1 = multiply(Aut2,Aut3,N,N,N,N,[], 1,1),
-    printMat(MultAB1,N,N,1),
+    NAut = multiply(M1,M2,R1,R2,C1,C2, [], 1,1),
+    printMat(NAut,R1,C1,1).
 
-    Sum2 = addMatrix(Sum1,MultAB1, N,N,1, []),
-    printMat(Sum2, N,N,1).
-
-    % I2 = 1,
-    % J2 = 1,
-    % N = R2/sqrt(P),   
-    % Aut2 = getSubMatrix(M2,R1,C1,I2,J2,P, (I2-1)*N, (J2-1)*N, []),
-    % io:fwrite("~w~n",[Aut2]),
-    % I3 = 1+1,
-    % J3 = 1,
-    % Aut3 = getSubMatrix(M1,R1,C1,I3,J3,P, (I3-1)*N, (J3-1)*N, []),
-    % io:fwrite("~w~n",[Aut3]).
-
-    % printMat(M2,R2,C2,1),
-    % Aut = multiply(M1,M2,R1,R2,C1,C2, [], 1,1),
-    % io:fwrite("~w~n",[Aut]).
     % halt(0).   
 
 
